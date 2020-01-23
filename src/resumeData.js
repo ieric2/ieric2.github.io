d353cbfe7f5f6ab504e1e72aded038a2ba0829ec
ieric2 <ieric@seas.upenn.edu>
2020-01-23 03:03:02 -0500

    Prototype

---
+++ src/resumeData.js
@@ -0,0 +1,80 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Eric Liu",
+    "role": "student at The University of Pennsylvania studying Computer science and Economics",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/eric-liu-03959214a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/ieric2",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a junior at The University of Pennsylvania pursuing both a Computer Science degree and an Economics degree. I am simulataneously pursuing a Masters in Computer Science",
+    "education":[
+      {
+        "UniversityName":"The University of Pennsylvania",
+        "specialization":"Masters in Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"BlinkReceipt",
+        "specialization":"Software Engineering Intern",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Penn Medicine",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp1",
+        "imgurl":"./projectImages/console.jpg",
+        "link":""
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp2",
+        "imgurl":"./projectImages/console.jpg",
+        "link":""
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp3",
+        "imgurl":"./projectImages/console.jpg",
+        "link":""
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp4",
+        "imgurl":"./projectImages/console.jpg",
+        "link":""
+      }
+    ]
+  }
+
+  export default resumeData
